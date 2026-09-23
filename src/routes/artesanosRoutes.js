156ed51548c3f38df603f7a393e50bfc74d4257d
GonzaloSantillan <lsantillangonzalo73@gmail.com>
2026-09-23 17:08:41 -0300

    importar controller mostrar artesanos

--- src/routes/artesanosRoutes.js
+++ src/routes/artesanosRoutes.js
@@ -1,11 +1,13 @@
 import express from 'express'
 import {
+    mostrarArtesanos,
     crearArtesano,
     actualizarArtesanoId
 } from '../controllers/artesanosController.js'
 
 const routes = express.Router()
 
+routes.get(`/`, mostrarArtesanos)
 routes.post(`/crear`, crearArtesano)
 routes.put(`/:id`, actualizarArtesanoId)
 
